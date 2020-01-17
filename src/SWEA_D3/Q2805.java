c264e3cbae9e68ebeb72ea7dbba5e0a2aace83dd
이현규 <sr9166@naver.com>
2020-01-18 00:28:34 +0900

    SWEA_D3 Q2805

--- src/SWEA_D3/Q2805.java
+++ src/SWEA_D3/Q2805.java
@@ -3,22 +3,8 @@ package SWEA_D3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Q2805 {
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static class Pair {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -36,27 +22,19 @@ public class Q2805 {
             }
 
             int ans = 0;
-            Pair curXY = new Pair(size/2, size/2);
-            Queue<Pair> queue = new LinkedList<>();
-            queue.add(curXY);
-            while(!queue.isEmpty()) {
-                curXY = queue.poll();
-                ans += arr[curXY.x][curXY.y];
 
-                for (int i = 0; i < 4; i++) {
-                    int x = curXY.x + dx[i];
-                    int y = curXY.y + dy[i];
-                    if(x >= 0 && x < size && y >= 0 && y < size) {
-                        if(x+y == size/2 || x+y == size/2*3 || ) {
-                            break;
-                        }
-                        else if(!visit[x][y] && arr[x][y] != 1) {
-                            queue.add(new Pair(x,y));
-                            visit[x][y] = true;
-                        }
+            for (int i = 0; i < size; i++) {
+                if(i <= size/2) {
+                    for (int j = 0; j < i * 2 + 1; j++) {
+                        ans+=arr[i][size/2 - i + j];
                     }
-                } // end for
-            } // end while
+                }
+                else {
+                    for (int j = 0; j < (size*2-1) - i*2; j++) {
+                        ans+=arr[i][i - size/2 + j];
+                    }
+                }
+            }
 
             System.out.println(String.format("#%d %d", TESTSIZE+1, ans));
         }
