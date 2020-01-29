69cafb6862d3412f4de2b16e4447ead0b8b30d0d
이현규 <sr9166@naver.com>
2020-01-29 22:07:52 +0900

    Q2441

---
+++ src/repeat/Q2441.java
@@ -0,0 +1,21 @@
+package repeat;
+
+import java.util.Scanner;
+
+public class Q2441 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        sc.close();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < size - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
