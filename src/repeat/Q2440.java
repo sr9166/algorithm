b5591ba24a6be8eb5f0bc1f0d875b2ed88479f20
이현규 <sr9166@naver.com>
2020-01-29 22:05:29 +0900

    Q2440

--- src/repeat/Q2440.java
+++ src/repeat/Q2440.java
@@ -1,6 +1,8 @@
+package repeat;
+
 import java.util.Scanner;
 
-public class main {
+public class Q2440 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
