af99420542f379c35352cea39d5bbb65f6f5c8db
이현규 <sr9166@naver.com>
2020-01-30 00:22:21 +0900

    Q11719

---
+++ src/basic/Q11719.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = "";
+        while((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+    }
+}
