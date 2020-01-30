8045d57c2c5d2234089b2bd67b9a2df5f1b9c0d3
이현규 <sr9166@naver.com>
2020-01-30 20:11:32 +0900

    Q2442

---
+++ src/repeat/Q2442.java
@@ -0,0 +1,21 @@
+package repeat;
+
+import java.util.Scanner;
+
+public class Q2442 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
